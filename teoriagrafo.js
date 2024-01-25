fb96cac88d330f79034f20989d99bbc697408ee5
Donttellmeimcute <24362190+donttellmeimcute@users.noreply.github.com>
2024-01-25 15:10:03 -0600

    Changes to be committed: 	modified:   teoriagrafo.js

--- teoriagrafo.js
+++ teoriagrafo.js
@@ -1,3 +1,32 @@
-//Teoria de grafos
+/*
+Teoria de grafos
 
-//una estructura de datos interconectada al menos a otra con aristas
+    una estructura de datos interconectada al menos a otra con aristas
+
+
+    Grafos conexos
+
+        Son los que tienen conexiones con aristas que pueda conectar a otro 
+        por medio de un camino que puede o no pasar por otros grafos.
+
+    Grafo Ciclico
+
+        Que hay un camino al que se pueda regresar sin regresar por el mismo camino.
+    
+    Trayectoria
+
+        ir de un lugar a otro.
+
+    Camino
+
+        trayectoria definida(o por aqui o por alla).
+
+    Circuito
+
+        Un camino o trayectoria cerrada.
+
+    Arbol
+
+        Grafo que es conexo pero no ciclico.
+    
+    
